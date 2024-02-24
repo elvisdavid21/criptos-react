4d82bc79b6f743d9f35fd99a11f8ec8091d06745
elvisdavid21 <79238237+elvisdavid21@users.noreply.github.com>
2024-02-24 01:43:16 -0500

    //Creación del componente formulario
    además, se instalo style components y se realizo la consulta a la API de criptos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,58 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+import Formulario from './components/Formulario'
+import ImagenCripto from './img/imagen-criptos.png'
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;//centrado de forma horizpntal
+  width: 90%;
+  //a partir de 992px se crean las 2 columnas con el @media
+  @media(min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+}
+`
+const Imagen = styled.img`
+  max-width: 400px;
+  width:80%;
+  margin: 100px auto 0 auto;
+  display: block;//las img por defecto siempre tienen un display: inlane
+`
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+  text-align:center;
+  font-weight: 700;//fuente importada desde google fonts
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after{
+    content: '*****';
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
 
 function App() {
 
   return (
-    <h1>
-      Criptominedas
-    </h1>
+    <Contenedor>
+      <Imagen
+        src={ImagenCripto}
+        alt='imagen criptomonedas'
+      />
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario />
+      </div>
+    </Contenedor>
   )
 }
 
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,77 @@
+import {useEffect, useState} from 'react'
+import styled from '@emotion/styled'
+import useSelectMonedas from '../hooks/useSelectMonedas'
+import Error from './Error'
+import {monedas} from './data/monedas'
+
+const InputSubmit = styled.input`
+    width: 100%;
+    padding: 10px;
+    background-color: #9497ff;
+    color: #FFF;
+    text-transform: uppercase;
+    font-size: 20px;
+    font-family: 'Lato', sans-serif;
+    font-weight: 700;
+    border-radius: 5px;
+    border: none;
+    transition: background-color .3s ease;
+    margin-top: 30px;
+    &:hover{
+        background-color: #7a7dfe;
+        cursor: pointer;
+    }
+`
+
+const Formulario = () => {
+
+    const [criptos, setCriptos] = useState([]);
+    const [error, setError] = useState(false)
+
+    //Parte del hooke creado
+    const [moneda, SeleccionMonedas] = useSelectMonedas('Elige tu moneda', monedas)
+    const [cripto, SeleccionCripto] = useSelectMonedas('Elige la Criptomoneda', criptos)
+
+    //Consulta a la API
+    useEffect( () => {
+      const consultarAPI = async() => {
+        const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json()
+
+        const arrayCriptos = resultado.Data.map( cripto => {
+          
+          return {id: cripto.CoinInfo.Name, nombre: cripto.CoinInfo.FullName}
+        })
+        setCriptos(arrayCriptos)
+
+      }
+      consultarAPI();
+    }, [])
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      if([moneda, cripto].includes('')) {
+        setError(true)
+        setTimeout( () => {
+          setError(false)
+        },3000)
+      }
+    } 
+
+  return (
+    <>
+      {error && <Error>Todos Los Campos son Obligatorios</Error>}
+      <form
+        onSubmit={handleSubmit}
+      >
+          <SeleccionMonedas />
+          <SeleccionCripto/>
+          <InputSubmit type="submit" value='Cotizar' />
+      </form>
+    </>
+  )
+}
+
+export default Formulario
